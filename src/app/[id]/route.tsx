abb5ebbfebcc00f78a0a1300f3f5f29bdcb6f191
Dennise Catolos <dentolos19@gmail.com>
2024-04-19 15:27:15 +0800

    Updated project

--- src/app/[id]/route.tsx
+++ src/app/[id]/route.tsx
@@ -1,9 +1,9 @@
 import { getRedirect } from "@/lib/database";
 
 export async function GET(
-	request: Request,
-	{ params }: { params: { id: string } },
+  request: Request,
+  { params }: { params: { id: string } }
 ) {
-	const data = await getRedirect(params.id);
-	return Response.redirect(data.url, 301);
+  const data = await getRedirect(params.id);
+  return Response.redirect(data?.url ?? "/", 301);
 }
--- src/lib/database.ts
+++ src/lib/database.ts
@@ -3,7 +3,7 @@ import json from "./database.json";
 export type Redirect = {
   description: string;
   url: string;
-}
+};
 
 export function getRedirect(id: string) {
   const data = json as Record<string, Redirect>;
