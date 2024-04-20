f670cedaaec3dd3bef669c9e60ab0493d4cfad46
Dennise Catolos <dentolos19@gmail.com>
2024-04-20 08:54:58 +0800

    Added basic API endpoint for retrieving data

---
+++ src/app/api/retrieve/route.ts
@@ -0,0 +1,17 @@
+import { getRedirect } from "@/lib/database";
+import type { NextRequest } from "next/server";
+
+export const revalidate = 0;
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const id = searchParams.get("id");
+  if (!id) {
+    return Response.json({ error: "ID is required" }, { status: 400 });
+  }
+  const data = await getRedirect(id);
+  if (!data) {
+    return Response.json({ error: "Not found" }, { status: 404 });
+  }
+  return Response.json(data, { status: 200 });
+}
