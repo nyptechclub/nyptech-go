1cd311d68c3dcc17cba12ae81b0d2362df555ca1
Dennise Catolos <dentolos19@gmail.com>
2024-04-30 18:06:11 +0800

    Fixed some issues

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,15 +5,23 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main className={"h-full flex items-center justify-center"}>
-      <div className={"text-center flex flex-col container break-words w-96 m-5 md:w-full"}>
+      <div
+        className={
+          "text-center flex flex-col container break-words w-96 m-5 md:w-full"
+        }
+      >
         <img
           className={"mb-4 size-[200px] mx-auto"}
           src={"https://nyptech.vercel.app/favicon.ico"}
           alt={"Icon"}
         />
-        <h1 className={"text-2xl font-bold break-words m-5"}>NYP Technopreneurship Club</h1>
+        <h1 className={"text-2xl font-bold break-words m-5"}>
+          NYP Technopreneurship Club
+        </h1>
         <p className={"text-base"}>
-        Develop an entrepreneurial mindset across the SIT student body through engagements and real-world problem solving with the application of technology.
+          Develop an entrepreneurial mindset across the SIT student body through
+          engagements and real-world problem solving with the application of
+          technology.
         </p>
 
         <div className={"mt-6"}>
