9ff474f8e244fdf8945283f21fc8ee8e1ae62d60
Dennise Catolos <dentolos19@gmail.com>
2024-07-29 22:25:47 +0800

    Moved admin dashboard to another repo

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -1,16 +1,21 @@
+import clsx from "clsx";
 import { Home, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 
-export default function NavigationBar(params: { className?: string }) {
+export default function NavigationBar(props: { className?: string }) {
   return (
-    <nav className={`navbar bg-base-300 ${params.className}`}>
+    <nav className={clsx("navbar bg-base-300", props.className)}>
       <div className={"navbar-start"}>
         <Link className={"btn btn-ghost"} title={"Home"} href={"/"}>
           <Home />
         </Link>
       </div>
       <div className={"navbar-end"}>
-        <Link className={"btn btn-ghost"} title={"Admin"} href={"/admin"}>
+        <Link
+          className={"btn btn-ghost"}
+          title={"Admin"}
+          href={"http://nyptech-admin.vercel.app/admin/microservices/go"}
+        >
           <LayoutDashboard />
         </Link>
       </div>
